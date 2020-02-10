2edee1c930d27f21a4029e3d332f4055c290b26d
qiankai <qiankai07@meituan.com>
2020-02-11 00:32:56 +0800

    process context中封装business code和product code，标记该context属于哪个业务场景

--- src/main/java/com/dangdang/framework/business/ProcessContext.java
+++ src/main/java/com/dangdang/framework/business/ProcessContext.java
@@ -14,8 +14,9 @@ public class ProcessContext {
 
     //定义当前执行阶段
     private String currentStage = ProcessStageEnum.INIT.name();
-
     private ProcessConfig processConfig;
+    private String businessCode;
+    private String productCode;
 
 
     public String getCurrentStage() {
@@ -32,5 +33,21 @@ public class ProcessContext {
 
     public void setProcessConfig(ProcessConfig processConfig) {
         this.processConfig = processConfig;
+    }
+
+    public String getBusinessCode() {
+        return businessCode;
+    }
+
+    public void setBusinessCode(String businessCode) {
+        this.businessCode = businessCode;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
     }
 }
