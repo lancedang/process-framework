791ee97eca4877393ec7bb9c799d2d02f9fd21b3
qiankai <qiankai07@meituan.com>
2020-02-10 11:38:46 +0800

    定义Router及DefaultRouter导航逻辑

---
+++ src/main/java/com/dangdang/framework/router/DefaultRouter.java
@@ -0,0 +1,27 @@
+// Copyright (C) 2020 Meituan
+// All rights reserved
+package com.dangdang.framework.router;
+
+import com.dangdang.framework.business.ProcessContext;
+import com.dangdang.framework.constant.ProcessStageEnum;
+import com.dangdang.framework.process.ProcessStage;
+
+/**
+ * @author qiankai07
+ * @version 1.0
+ * Created on 2/9/20 8:22 PM
+ **/
+public class DefaultRouter implements Router {
+    public void route(ProcessContext processContext) {
+        String currentStage = processContext.getCurrentStage();
+        if (ProcessStageEnum.INIT.name().equals(currentStage)) {
+            processContext.setCurrentStage(ProcessStageEnum.PROCESS.name());
+        } else if (ProcessStageEnum.PRE_PROGRESS.name().equals(currentStage)) {
+            processContext.setCurrentStage(ProcessStageEnum.PROCESS.name());
+        } else if (ProcessStageEnum.PROCESS.name().equals(currentStage)) {
+            processContext.setCurrentStage(ProcessStageEnum.POST_PROCESS.name());
+        } else if (ProcessStageEnum.POST_PROCESS.name().equals(currentStage)) {
+            processContext.setCurrentStage(ProcessStageEnum.FINISH.name());
+        }
+    }
+}
--- src/main/java/com/dangdang/framework/router/Router.java
+++ src/main/java/com/dangdang/framework/router/Router.java
@@ -1,4 +1,7 @@
 package com.dangdang.framework.router;
 
+import com.dangdang.framework.business.ProcessContext;
+
 public interface Router {
+    void route(ProcessContext processContext);
 }
