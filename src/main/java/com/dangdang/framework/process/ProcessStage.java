33b8dc9c4e1e3a1a9e57a990371f274725b0881c
qiankai <qiankai07@meituan.com>
2020-02-10 11:34:48 +0800

    ProcessStage封装相应阶段所有actions及该阶段对应Router

---
+++ src/main/java/com/dangdang/framework/process/ProcessStage.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 Meituan
+// All rights reserved
+package com.dangdang.framework.process;
+
+import com.dangdang.framework.business.BusinessAction;
+
+import java.util.List;
+
+/**
+ * ProcessStage封装：
+ * 1.每个阶段包含的Action（子任务）；
+ * 2.每个阶段所使用的Router（Router作用是不通Stage之间的导航器，比如从INIT阶段转到PROGRESS阶段，当然导航逻辑自定义）
+ *
+ * @author qiankai07
+ * @version 1.0
+ * Created on 2/9/20 9:04 PM
+ **/
+public class ProcessStage {
+    private List<BusinessAction> actions;
+
+    private String router = "defaultRouter";
+
+    public List<BusinessAction> getActions() {
+        return actions;
+    }
+
+    public void setActions(List<BusinessAction> actions) {
+        this.actions = actions;
+    }
+
+    public String getRouter() {
+        return router;
+    }
+
+    public void setRouter(String router) {
+        this.router = router;
+    }
+}
