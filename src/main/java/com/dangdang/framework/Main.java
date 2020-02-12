636c3f3b85a259ad27bc4f026aa4a055836d3df2
qiankai <qiankai07@meituan.com>
2020-02-12 12:36:06 +0800

    Main添加SpringBoot引导类，用于封装Spring上下文环境（方便测试）

--- src/main/java/com/dangdang/framework/Main.java
+++ src/main/java/com/dangdang/framework/Main.java
@@ -1,7 +1,13 @@
 package com.dangdang.framework;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(locations = {"classpath:process-executor.xml", "classpath:process-action-bean.xml"})
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        SpringApplication.run(Main.class, args);
     }
 }
